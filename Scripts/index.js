277cea53491b7799ceafc015731f9b0e22c74cb5
Ibrahim11511 <109541964+Ibrahim11511@users.noreply.github.com>
2024-07-05 22:59:55 +0300

    add hero section and complete the responsive

--- Scripts/index.js
+++ Scripts/index.js
@@ -40,3 +40,69 @@ overLayer.addEventListener("click", () => {
 window.addEventListener("load", function () {
   loader.classList.add("loaded");
 });
+
+const heroSlider = document.querySelector(".hero-slider");
+const heroSliderItems = document.querySelectorAll(".slider-item");
+const heroSliderPrevBtn = document.querySelector(".slider-prev-btn");
+const heroSliderNextBtn = document.querySelector(".slider-next-btn");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function () {
+  lastActiveSliderItem.classList.remove("active");
+  heroSliderItems[currentSlidePos].classList.add("active");
+  lastActiveSliderItem = heroSliderItems[currentSlidePos];
+};
+
+const slideNext = function () {
+  if (currentSlidePos >= heroSliderItems.length - 1) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  updateSliderPos();
+};
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = heroSliderItems.length - 1;
+  } else {
+    currentSlidePos--;
+  }
+
+  updateSliderPos();
+};
+
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * auto slide
+ */
+
+let autoSlideInterval;
+
+const autoSlide = function () {
+  autoSlideInterval = setInterval(function () {
+    slideNext();
+  }, 7000);
+};
+
+// addEventOnElements(
+//   [heroSliderNextBtn, heroSliderPrevBtn],
+//   "mouseover",
+//   function () {
+//     clearInterval(autoSlideInterval);
+//   }
+// );
+
+// addEventOnElements(
+//   [heroSliderNextBtn, heroSliderPrevBtn],
+//   "mouseout",
+//   autoSlide
+// );
+
+window.addEventListener("load", autoSlide);
